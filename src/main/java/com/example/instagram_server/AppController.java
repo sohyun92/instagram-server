02188b8c3061126f5295c6703002b1e1f42c3ee7
0830thgus@naver.com <0830thgus@naver.com>
2024-10-22 18:58:47 +0900

    first commit

---
+++ src/main/java/com/example/instagram_server/AppController.java
@@ -0,0 +1,15 @@
+package com.example.instagram_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+	@GetMapping("/")
+	public String home(){
+		return "Hello, world";
+	}
+}
